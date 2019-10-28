8cb2d1ae50ea99a2ad2a9167bb3c613f3bd4d971
davim5 <davim53@hotmail.com>
2019-10-28 17:28:59 -0300

    Created first migration and seed

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
 import express from 'express';
+import routes from './routes';
 
-class App{
-  constructor(){
+class App {
+  constructor() {
     this.server = express();
-
+    this.middlewares();
+    this.routes();
   }
 
-  middlewares(){
-    this.server.use(express.json())
+  middlewares() {
+    this.server.use(express.json());
   }
 
-  routes(){
-    this.server.use(this.routes)
+  routes() {
+    this.server.use(routes);
   }
-
 }
 
-export default new App().server
+export default new App().server;
